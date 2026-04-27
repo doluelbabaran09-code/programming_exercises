30dc038469bae3b4e9c9260dec6774cfda5cbd68
doluelbabaran09 <dolueelbabaran09@gmail.com>
2026-04-28 02:12:05 +0800

    implement PowerProcessor sub-class with math logic

--- mathematical_power_processor/power_calculator.py
+++ mathematical_power_processor/power_calculator.py
@@ -5,4 +5,27 @@ class MathBase:
         self.target_file_name = target_file_name
     def is_file_ready(self):
         return os.path.exists(self.target_file_name)
-    
+
+class PowerProcessor(MathBase):
+    def _init__(self):
+        super().__init__("mathematical_power_processor/integers.txt")
+    def calculate_powers(self):
+         try:
+             if not self.is_file_ready():
+                raise FileNotFoundError(f"{self.target_file_name} is missing, ")
+
+            with open(self.target_file_name, "r") as input_file:
+                integer_list = [int(line.strip()) for line in if line.strip()]
+
+            with open("double.txt", "w") as square_file, open("triple.txt", "w") as cube_file:
+                for current_integer in integer_list:
+                    if current_integer % 2 == 0:
+                        square_file.write(f"{current_integer ** 2}\n")
+                    else:
+                        cube_file.write(f"{current_integer ** 3}\n")
+            print("Successfully processed the integers annd saved the results to double.txt and triple.txt.")
+         
+        except ValueError:
+            print("Error: integers.txt must contaio only valid integers.")
+
+
