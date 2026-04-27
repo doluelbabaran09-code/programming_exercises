bd2975000f384bd88edc90570cd8cf8a385aa50f
doluelbabaran09 <dolueelbabaran09@gmail.com>
2026-04-28 01:16:41 +0800

    fix the indent and other spelling, also changed w to a so it wont rewrite every time its used

--- Daily_Journal_Application/life_diary_writer.py
+++ Daily_Journal_Application/life_diary_writer.py
@@ -3,19 +3,23 @@ class JournalBase:
         self.target_file_name = target_file_name
     def write_journal_content(self):
         pass
-class MultipleLineDiary(JounalBase)
+class MultipleLineDiary(JournalBase):
     def __init__(self):
-        super().__init__("Daily_Journal_Application/my_lifet.txt")
-    def write_journal_contenttt(self):
-        with open(self.target_file_name, "w") as output_file:
+        super().__init__("Daily_Journal_Application/mylife.txt")
+    def write_journal_content(self):
+        with open(self.target_file_name, "a") as output_file:
             continue_writing_input = True
-        while continue_writing_input:
-            user_input = input("Enter your line: ")
-            output_file.write(user_input + "\n")
 
-            user_repeat_choice = input ("Do you want to add another line? (yes/no): ") lower()
-            if user_repeat_choice != "y":
-                continue_writing_input = False
+            while continue_writing_input:
+                user_input = input("Enter your line: ")
+                output_file.write(user_input + "\n")
+
+                user_repeat_choice = input("Do you want to add another line? (yes/no): ").lower()
+                if user_repeat_choice != "y":
+                     continue_writing_input = False
 
         print(f"Content successfully written to {self.target_file_name}.")
 
+if __name__ == "__main__":
+    diary_instance = MultipleLineDiary()
+    diary_instance.write_journal_content()
