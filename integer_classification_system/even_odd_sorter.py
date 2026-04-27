ab258e72b72dc2c4f39796eb1aafe585518da943
doluelbabaran09 <dolueelbabaran09@gmail.com>
2026-04-27 18:38:48 +0800

    implementing is_file_is_ready method in FileValidator

--- integer_classification_system/even_odd_sorter.py
+++ integer_classification_system/even_odd_sorter.py
@@ -4,3 +4,7 @@ class FileValidator:
     """Base class to handle file existence checks."""
     def __init__(self, target_file_name):
         self.target_file_name = target_file_name
+    def is_file_ready(self):
+        """Behavior: Checks if the source file exists."""
+        return os.path.exists(self.target_file_name)
+    
