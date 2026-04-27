60115f1869f243a3aaa60b1c5b010022fb7ce31d
doluelbabaran09 <dolueelbabaran09@gmail.com>
2026-04-28 00:02:03 +0800

    define FileValidator parent class

---
+++ student_performance_analyzer/gwa_record_analyzer.py
@@ -0,0 +1,8 @@
+import os
+
+class FileValidator:
+    def __init__(self, target_file_name):
+        self.target_file_name = target_file_name 
+
+    def is_file_ready(self):
+        return os.path.exists(self.target_file_name)
